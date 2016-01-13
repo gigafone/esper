b288994f3c7b5e07b18a921fb0dd9f4a58d3c69d
gigafone <gigafone>
2016-01-13 11:52:13 -0800

    improve readability of benchmark scripts

--- examples/benchmark/runClient.sh
+++ examples/benchmark/runClient.sh
@@ -1,21 +1,41 @@
-# uncomment and set your JAVA_HOME
-#JAVA_HOME=""
+#!/usr/bin/env bash
+set -eu
 
-# the classpath
-# you need to get an Esper distribution separately from the benchmark kit
-LCP=../../esper/target/classes:../../esper/lib/commons-logging-1.1.3.jar:../../esper/lib/cglib-nodep-3.1.jar:../../esper/lib/antlr-runtime-4.1.jar:../../esper/lib/log4j-1.2.17.jar
-CP="etc:bin:$LCP:lib/esper-5.3.0.jar:lib/commons-logging-1.1.3.jar:lib/cglib-nodep-3.1.jar:lib/antlr-runtime-4.1.jar:lib/log4j-1.2.17.jar"
+# Use this to set your JAVA_HOME.
+# (Some prefer to set this in their shell.)
+# JAVA_HOME=""
+
+# Local Java Classpath
+# (Includes local Esper build results, if present)
+ESPER="../../esper"
+LCP="$ESPER/target/classes"\
+":$ESPER/lib/commons-logging-1.1.3.jar"\
+":$ESPER/lib/cglib-nodep-3.1.jar"\
+":$ESPER/lib/antlr-runtime-4.1.jar"\
+":$ESPER/lib/log4j-1.2.17.jar"
+
+# Java Classpath
+# Checks for a local Esper build (see $LCP above) or a JAR in ./lib
+CP="etc:bin"\
+":$LCP"\
+":lib/esper-5.3.0.jar"\
+":lib/commons-logging-1.1.3.jar"\
+":lib/cglib-nodep-3.1.jar"\
+":lib/antlr-runtime-4.1.jar"\
+":lib/log4j-1.2.17.jar"
 
 # JVM options
 OPT="-Xms128m -Xmx128m"
 
-# rate
+# Rate
 RATE="-rate 10000"
 
 # remote host, we default to localhost and default port
 HOST="-host 127.0.0.1"
 
-$JAVA_HOME/bin/java $OPT -classpath $CP -Desper.benchmark.symbol=1000 com.espertech.esper.example.benchmark.client.Client $RATE $HOST
-
-
-
+$JAVA_HOME/bin/java $OPT\
+ -classpath $CP\
+ -Desper.benchmark.symbol=1000\
+ com.espertech.esper.example.benchmark.client.Client\
+ $RATE $HOST\
+ 2>&1 | tee benchmark-client.log
